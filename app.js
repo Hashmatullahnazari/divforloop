6062fb6e453a82c4a76e3e040c9c53c4346e8823
Hashmatullah Nazari <hn0153@gmail.com>
2022-05-11 15:34:32 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var container = '<div><p>Lorefugiat eum voluptatibus cupiditate excepturi unde eius. Ducimus modi quos mollitia.</p></div>';
+var text ='';
+for (var i = 0; i < 6; i++) {
+  text += container;
+}
+
+document.getElementById("output").innerHTML = text;
+
